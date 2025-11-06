236318581d42d0535e829dfe7098d9c591ea7f03
aayush-dev20 <aayushkrdev.in@gmail.com>
2025-11-06 22:05:35 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,40 @@
+const kits = ["crash","kick-bass","snare(1)","snare","tom-1","tom-2","tom-3","tom-4"]
+
+const containerEl = document.querySelector(".container");
+
+kits.forEach((kit) => {
+  const btnEl = document.createElement("button");
+  btnEl.classList.add("btn");
+  btnEl.innerText = kit;
+
+  // Add background image
+  btnEl.style.backgroundImage = 'images/" + kit + ".png';
+
+  // Create audio element
+  const audioEl = document.createElement("audio");
+  audioEl.src = "audio/" + kit + ".mp3";
+
+  // Play sound on click
+  btnEl.addEventListener("click", () => {
+    audioEl.play();
+  });
+
+  // Add button and audio
+  containerEl.appendChild(btnEl);
+  containerEl.appendChild(audioEl);
+
+  btnEl.addEventListener("click" , ()=>{
+    audioEl.play();
+  })
+  window.addEventListener("keydown",(Event)=>{
+   if(event.key === kit.slice(0,1)){
+    audioEl.play()
+    btnEl.styletransform = "scale(.9)";
+    setTimeout(()=>{
+        btnEl.style.transform = "scale(1)";
+
+    }, 100);
+   }
+    }
+  )
+});
